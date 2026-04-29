8f5d8f64c75ee09c8fbd23fbbe85d203c50aed0a
mat-dias <slender.adobe@gmail.com>
2026-04-29 01:35:58 -0300

    Delete php directory

--- php/senha_email.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Email usado no envio Corrigido
-define('PWD', 'gbweafplrvrdqlze');
-define('USER', 'matheusdeoliveiramoreira2@gmail.com');
-?>
--- php/teste_conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "MySQLClass.php";
-try {
-    $db = getConexao();
-    echo "Conectado com sucesso ao servidor DBaaS!";
-} catch (Exception $e) {
-    echo "Erro de conexão: " . $e->getMessage();
-}
