2bc4f76032053a22bbff969abc215343a4f712bf
Oliver_Dev_001 <matheusdeoliveiramoreira2@gmail.com>
2026-06-03 15:48:38 -0300

    Update senha_email.php

--- focus1.8/Focus1.6/Front_Integrar/Pages/php/senha_email.php
+++ focus1.8/Focus1.6/Front_Integrar/Pages/php/senha_email.php
@@ -1,5 +1,28 @@
 <?php
-// Email usado no envio Corrigido
-define('PWD', 'gbweafplrvrdqlze');
-define('USER', 'matheusdeoliveiramoreira2@gmail.com');
+if (!function_exists('loadEnv')) {
+    function loadEnv($path)
+    {
+        if (!file_exists($path)) {
+            throw new Exception("Nenhum arquivo .env Detectado em: " . $path);
+        }
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) continue;
+            if (strpos($line, '=') !== false) {
+                list($name, $value) = explode('=', $line, 2);
+                $_ENV[trim($name)] = trim($value);
+                putenv(trim($name) . "=" . trim($value));
+            }
+        }
+    }
+}
+
+try {
+    loadEnv(__DIR__ . '/../../.env');;
+} catch (Exception $e) {
+}
+
+// Define as constantes
+define('USER', getenv('USER') ?: ($_ENV['USER'] ?? ''));
+define('PWD', getenv('PWD') ?: ($_ENV['PWD'] ?? ''));
 ?>
